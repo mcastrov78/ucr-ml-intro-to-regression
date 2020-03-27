e6ce4ee136bb99cb88f6827558fd36c008c000b8
Mauricio Castro <mauricio.castro@avantica.net>
2020-03-27 14:12:23 -0600

    Adding data file

--- intro_to_regression.R
+++ intro_to_regression.R
@@ -1,6 +1,7 @@
-download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
+#download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
 load("mlb11.RData")
 #help(load)
+#?help(load)
 
 cor(mlb11$runs, mlb11$at_bats)
 cor(mlb11$at_bats, mlb11$runs)
